6953f1c04286f393fb34de7e2fcc8ffd37ac4e1d
iluvjava <victor1301166040@gmail.com>
2021-07-29 13:06:54 -0700

    d

--- func_utils.cpp
+++ func_utils.cpp
@@ -2,18 +2,29 @@
 #include <pybind11/stl.h>
 #include <iostream>
 
+
+namespace py = pybind11;
+
 /**
  * 
  */
 double MaxRecursive(std::vector<double>& arr, int start=0, int end=-1)
 {
-    
-
+    return 0.0;
 }
 
+py::int_ add(py::int_ a, py::int_b)
+{
+    return a + b;
+}
 
 
 // -----------------------------------------------------------------------------
 // Python Interfacing code
 // -----------------------------------------------------------------------------
 
+PYBIND11_MODULE(cppbings, m)
+{
+    m.doc() = "binded cpp functions"; 
+    m.def("add", &add);
+}
