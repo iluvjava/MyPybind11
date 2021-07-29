9dfb6e57a99d9682b1137e8df5e769831b824bd3
iluvjava <victor1301166040@gmail.com>
2021-07-29 13:50:52 -0700

    delete corrupt

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
-import cppbings
+import cppbindings
 
-print(f"binded module: {cppbings}")
-print(f"binded function: {cppbings.add}")
-print(f"9999999999999999 + 9999999999999999 = {cppbings.add(9999999999999999,  9999999999999999)}")
+print(f"binded module: {cppbindings}")
+print(f"binded function: {cppbindings.add}")
+print(f"9999999999999999 + 9999999999999999 = {cppbindings.add(9999999999999999,  9999999999999999)}")
